6c66bfd50cf2eaea732de8b5f479acd018a3fbbf
Eric Moran <emoran0403@gmail.com>
2022-02-02 16:31:48 -0500

    add die and sum buttons working

--- app.js
+++ app.js
@@ -5,6 +5,10 @@
  * When a new Die is created, it should automatically create a div, call its roll method, and put the div on the screen
  * You can use either the DOM or JQuery, but please be consistent and stick with one.
  * 
+ * 
+ * 
+ //! make it so the die container on the page starts out big enough to not require a resize when die are put on the screen
+ * 
  * ⚀ die face-1. U+2680.
 ⚁ die face-2. U+2681.
 ⚂ die face-3. U+2682.
@@ -29,9 +33,12 @@ let currentDice = [];
 
 class Die {
   // creates a class called Die
-  static id = 0; // this will eventually be used to give each die a unique id
+
+  static id = 1; // this will eventually be used to give each die a unique id
+
   constructor() {
-    this.value = 146; // sets the default value to undefined
+    this.value = undefined; // sets the default value to undefined
+    this.id = Die.id++; // gives each die a unique id when it is instantiated
   }
   roll() {
     // sets the new value to a random int 1-6 inclusive
@@ -46,30 +53,52 @@ $(`#newdie`).click(function () {
    * needs to create a new die object //?done
    * needs to make a die display inside dieContainer//?done
    * needs to make that die show its current value//? done
-   * needs to add it to to currentDice array
+   * needs to add it to to currentDice array//?done
    */
 
   let myDie = new Die(); // myDie is a new instance of the Die class
-  myDie.roll();
-  let myPageDie = $(`<div>`) // puts a representation of the myDie instance on the page
-    .text(myDie.value) // with the value as text
-    .addClass("die border border-dark border-4 rounded"); //and a die-ish display    //! need to make them look more like dice
+  myDie.roll(); //  sets the initial value of the die from undefined to a random die value
 
-  $(myPageDie).appendTo(dieContainer); // appends the die representation to the die container div
-  // console.log(`my die is ${myDie}`); //*logging
+  //sets styles and text value
+  let mydieholder = $(`<div class="col-1 my-3">`); // this div holds the die
+  let myPageDie = $(`<div class="die py-3">`) // this div is the representation of the die
+    .text(myDie.value)
+    .addClass(
+      "die border border-dark border-4 rounded d-flex justify-content-center align-items-center"
+    )
+    .attr("id", myDie.id); // matches the die representation id to be the same as the id of the object die
+
+  //appends the die to the page
+  $(myPageDie).appendTo(mydieholder);
+  $(mydieholder).appendTo(dieContainer);
+
+  console.log(myDie); //*logging
   // console.log(`the die's value is ${myDie.value}`); //*logging
 
-  currentDice.push(myDie); // adds the die to the currentDice array
-  //console.log(currentDice); //*logging
+  // adds the die to the currentDice array
+  currentDice.push(myDie);
+
+  console.log(currentDice); //*logging
 });
 
 $(`#reroll`).click(function () {
-  console.log(`reroll was pressed`);
   /**
    * needs to select all die in the currentDice array and call the roll method on each of them
-   *
-   *
+   * for Each array method, or a for loop should work here
    */
+
+  console.log(`reroll was pressed`);
+  let tempvar = currentDice[0];
+  tempvar.roll();
+  console.log(currentDice);
+
+  // currentDice.forEach(() => {
+  //   console.log(`my for Each is working!`); //*logging
+  //   for (let i = 0; i < currentDice.length; i++) {
+  //     console.log(`my for loop is working`);
+  //     currentDice[i].roll();
+  //   }
+  // });
 });
 
 $(`#sum`).click(function () {
@@ -80,6 +109,11 @@ $(`#sum`).click(function () {
    *
    *
    */
+  let sum = 0;
+  for (let i = 0; i < currentDice.length; i++) {
+    sum += currentDice[i].value;
+  }
+  alert(sum);
 });
 
 $(`#pip`).click(function () {
