ade0d736315e92fad187f407e69a5208552552f6
Eric Moran <emoran0403@gmail.com>
2022-02-03 19:49:31 -0500

    adding new die, and rerolling die both respect the states of pipmode

--- app.js
+++ app.js
@@ -20,7 +20,12 @@
 
 let dieContainer = $(`#dieContainer`);
 
+let pipsActive = false;
+
+let allDieObject = document.getElementsByClassName("die"); // this is an array of objects - use: allDieObject[i].innerText
+
 let currentDice = [];
+
 /**
  * this will serve as an array for the current dice.
  * summing will be done via loop or array methods, each calling on the value proprerty of each die.
@@ -45,7 +50,7 @@ class Die {
 
 // generates a new die instance, stores that object in the currentDice array, and displays a representation of that on the page
 $(`#newdie`).click(function () {
-  console.log(`newdie was pressed`);
+  // console.log(`newdie was pressed`); //*logging
 
   let myDie = new Die(); // myDie is a new instance of the Die class
   myDie.roll(); //  sets the initial value of the die from undefined to a random die value
@@ -53,23 +58,49 @@ $(`#newdie`).click(function () {
   //sets styles and text value
   let mydieholder = $(`<div class="dieParent col-1 my-3">`); // this div holds the die
   let myPageDie = $(`<div class="die py-3">`) // this div is the representation of the die
-    .text(myDie.value)
     .addClass(
       "die border border-dark border-4 rounded d-flex justify-content-center align-items-center"
     )
     .attr("id", myDie.id); // matches the die representation id to be the same as the id of the object die
 
+  // displays the value of new dice on screen as pips or numbers depending on the state of pipsActive
+  if (pipsActive) {
+    console.log(myDie.value);
+    switch (myDie.value.toString()) {
+      case `1`:
+        myPageDie.text(`⚀`);
+        break;
+      case `2`:
+        myPageDie.text(`⚁`);
+        break;
+      case `3`:
+        myPageDie.text(`⚂`);
+        break;
+      case `4`:
+        myPageDie.text(`⚃`);
+        break;
+      case `5`:
+        myPageDie.text(`⚄`);
+        break;
+      case `6`:
+        myPageDie.text(`⚅`);
+    }
+  } else {
+    myPageDie.text(myDie.value);
+  }
+  console.log(currentDice);
+
   //appends the die to the page
   $(myPageDie).appendTo(mydieholder);
   $(mydieholder).appendTo(dieContainer);
 
-  console.log(myDie); //*logging
+  // console.log(myDie); //*logging
   // console.log(`the die's value is ${myDie.value}`); //*logging
 
   // adds the die to the currentDice array
   currentDice.push(myDie);
 
-  console.log(currentDice); //*logging
+  // console.log(currentDice); //*logging
 });
 
 // loops through the currentDice array, calls the reroll method on each die, and updates the display
@@ -93,9 +124,21 @@ $(`#sum`).click(function () {
   alert(sum);
 });
 
-let allDieObject = document.getElementsByClassName("die"); // this is an array of objects - use: allDieObject[i].innerText
-
 $(`#pip`).click(function () {
+  // this toggles the pip display
+  if (pipsActive) {
+    pipToNum();
+    pipsActive = false;
+  } else {
+    numToPip();
+    pipsActive = true;
+  }
+});
+
+function numToPip() {
+  // this swaps all numbers to pips
+  pipsActive = true;
+
   for (i = 0; i < allDieObject.length; i++) {
     let textCompare = allDieObject[i].innerText;
 
@@ -119,18 +162,33 @@ $(`#pip`).click(function () {
         allDieObject[i].innerText = `⚅`;
     }
   }
-});
+}
 
-/**
- *
- *
- * if pip mode = true
- * then new die must get their numerical value on the object
- * and display their pips
- * old die must swap to pips
- *
- * if pip mode = false
- * then new die must display their numerical value on the object
- * display their number
- * old die must swap to numbers
- */
+function pipToNum() {
+  // this swaps all pips to numbers
+  pipsActive = false;
+
+  for (i = 0; i < allDieObject.length; i++) {
+    let textCompare = allDieObject[i].innerText;
+
+    switch (textCompare) {
+      case `⚀`:
+        allDieObject[i].innerText = `1`;
+        break;
+      case `⚁`:
+        allDieObject[i].innerText = `2`;
+        break;
+      case `⚂`:
+        allDieObject[i].innerText = `3`;
+        break;
+      case `⚃`:
+        allDieObject[i].innerText = `4`;
+        break;
+      case `⚄`:
+        allDieObject[i].innerText = `5`;
+        break;
+      case `⚅`:
+        allDieObject[i].innerText = `6`;
+    }
+  }
+}
